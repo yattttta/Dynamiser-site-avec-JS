f1ef14a6550f2e441d054f8139753962c0a6a213
yaTTTa <patate_o_gruyere@hotmail.fr>
2022-03-10 18:41:46 +0100

    Layout #3 - Layout général 1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+//$(document).ready(() => {
+
+// NEW GAME et réinitialisation des scores
+//const newGame = document.getElementById('newGame')
+//let scores = [$('#globalScore1'), $('#globalScore2'), $('#currentScore1'), $('#currentScore2')]
+//function restart() {
+  //  scores.forEach((elements) => {
+        //elements.html('0')
+    //})
+//}
+//newGame.addEventListener('click', restart)
+
+
+
+
+//})
